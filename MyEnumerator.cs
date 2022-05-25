24c577ecbb299c7956ce8f1121313692185f0430
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-05-25 18:24:20 +0400

    Add files via upload

---
+++ MyEnumerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    public class MyEnumerator
+    {
+        private int count;
+        _List list;
+        public MyEnumerator(_List collection)
+        {
+            list = collection;
+            count = -1;
+        }
+        public bool MoveNext()
+        {
+            count++;
+            return count < list._items.Length;
+        }
+
+        public int Current => list._items[count];
+    }
+
+}
+
